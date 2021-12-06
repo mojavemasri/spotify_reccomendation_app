b454e33d3ec33420b065d25afa18c993b7d98068
Youssef Hegab <hegab@chapman.edu>
2021-12-05 21:10:57 -0800

    updates to helper and app

--- pythoncode/app.py
+++ pythoncode/app.py
@@ -1,6 +1,7 @@
 from helper import helper
 from apihelper import apihelper
 from printRecord import printRecord
+from enterRecord import enterRecord
 
 def startscreen():
     print("")
@@ -43,34 +44,114 @@ def viewlibrary():
         Would you like to:
         1)Enter the Spotify url to display your track
         2)Enter the name of the song and search for it''')
-        searchChoice = helper.get_choice([1,2,3,4,5])
+        searchChoice = helper.get_choice([1,2])
         if searchChoice == 1:
-            url = ""
-            while True
-                url = input("Enter url:")
-                if helper.checkURL(url, 1):
-                    break
-                else:
-                    print("Invalid url, please try again")
-            trackID = url[url.index('track/')+6:url.index('?') ]
+            trackID = helper.getURLFromUser(1)
             printRecord.printFancyTrack(trackID)
         if searchChoice == 2:
             searchName = input("Enter name:")
-            #query resulting in list of all spotifyTrackIDs of records containing similar name
+
+            queryResult = searchDB(searchName, 1) #search DB needs to be implemented
+
+            for q in queryResult:
+                printRecord.printSimpleTrack(q[0])
+            if len(queryResult) == 0:
+                print("Sorry, no songs of this record were found. Please try again.")
+
+    elif typechoice == 2:
+        print('''
+        Would you like to:
+        1)Enter the album Spotify url to display your album
+        2)Enter an artist Spotify url and print all of their (database) albums
+        3)Enter the name of the album and search for it''')
+        searchChoice = helper.get_choice([1,2,3])
+        if searchChoice == 1:
+            albumID = helper.getURLFromUser(2)
+            printRecord.printFancyAlbum(albumID)
+        elif searchChoice == 2:
+            artistID = helper.getURLFromUser(3)
+            #write query to get each album where this artist is the artist
             queryResult = []
             for q in queryResult:
-                printRecord.printSimpleTrack(trackID)
-    elif typechoice == 2:
+                printRecord.printFancyAlbum(q[0])
+        elif searchChoice == 3:
+            searchName = input("Enter name:")
+
+            queryResult = searchDB(searchName, 2) #search DB needs to be implemented
+            print("SEARCH RESULTS: ")
+            for q in queryResult:
+                printRecord.printSimpleAlbum(q[0])
+            if len(queryResult) == 0:
+                print("Sorry, no albums witht this name were found. Please try again.")
     elif typechoice == 3:
+        print('''
+        Would you like to:
+        1)Enter the Spotify url to display your artist
+        2)Enter the name of the artist and search for it''')
+        searchChoice = helper.get_choice([1,2])
+        if searchChoice == 1:
+            artistID = helper.getURLFromUser(3)
+            printRecord.printFancyArtist(artistID)
+        if searchChoice == 2:
+            searchName = input("Enter name:")
+
+            queryResult = searchDB(searchName, 3) #search DB needs to be implemented
+            print("SEARCH RESULTS: ")
+            for q in queryResult:
+                printRecord.printSimpleArtist(q[0])
+            if len(queryResult) == 0:
+                print("Sorry, no artists with this name were found. Please try again.")
     elif typechoice == 4:
+        name = input("Enter a name of a genre to search for it here")
+        #WRITE A QUERY THAT DISPLAYS SIMILAR NAMES
+        queryResult = []
+        print("SEARCH RESULTS: ")
+        for q in queryResult:
+            printRecord.printSimpleGenre(q[0])
     elif typechoice == 5:
+        print('''
+        Would you like to:
+        1)Enter the Spotify url to display your playlist
+        2)Enter the name of the playlist and search for it''')
+        searchChoice = helper.get_choice([1,2])
+        if searchChoice == 1:
+            playlistID = helper.getURLFromUser(5)
+            printRecord.printFancyPlaylist(playlistID)
+        if searchChoice == 2:
+            searchName = input("Enter name:")
+
+            queryResult = searchDB(searchName, 5) #search DB needs to be implemented
+            print("SEARCH RESULTS: ")
+            for q in queryResult:
+                printRecord.printSimpleArtist(q[0])
+            if len(queryResult) == 0:
+                print("Sorry, no playlist with this name were found. Please try again.")
 
     print("Redirecting back to main menu, keep track of any useful information")
     input("Press any key to continue")
     pass
 
 def editlibrary():
-
+    print('''
+    Would you like to:
+    1)Insert a record
+    2)Update a record
+    3)Delete a record
+    ''')
+    typechoice = helper.get_choice([1,2,3])
+    if typechoice == 1:
+        print('''
+        Would you like to:
+        1)Enter a song
+        2)Enter an album
+        3)Enter a playlist
+        ''')
+        mediumchoice = helper.get_choice([1,2,3])
+        if mediumchoice == 1:
+        elif mediumchoice == 2:
+        elif mediumchoice == 3:
+    elif typechoice == 2:
+    elif typechoice == 3:
 def getreccomendations():
 
 def menuoptions():
--- pythoncode/helper.py
+++ pythoncode/helper.py
@@ -40,6 +40,65 @@ class helper():
         else:
             return False
 
+    #its in the name lol
+    @staticmethod
+    def convertURLtoURI(url):
+        if url[25:30] == "track" or url[25:30] == "album":
+            return url[32:url.index(?)]
+        elif url[25:31] == "artist":
+            return url[33:url.index(?)]
+        elif url[25:33] == "playlist":
+            return url[35:url.index(?)]
+        else:
+            return ""
+
+    #gets a url from user and returns the ID
+    # 1: track
+    # 2: album
+    # 3: artist
+    # 4: playlist
+    @staticmethod
+    def getURLFromUser(itemtype):
+        while True
+            url = input("Enter url:")
+            if helper.checkURL(url, itemtype):
+                break
+            else:
+                print("Invalid url, please try again")
+        return convertURLtoURI(url)
+
+    #takes in a spotify url and an item type and returns if it is a valid url
+    # ITEM TYPES
+    # 1: track
+    # 2: album
+    # 3: artist
+    # 4: playlist
+    # 5: genre
+    # SEARCH TYPES
+    # 1: name
+
+
+    @staticmethod
+    def searchDB(searchTerm, itemtype):
+        queryItem = ""
+        searchItem = ""
+        if itemtype == 1:
+            queryItem = 'track'
+        elif itemtype == 2:
+            queryItem = 'album'
+        elif itemtype == 3:
+            queryItem = 'artist'
+        elif itemtype == 4:
+            queryItem = 'playlist'
+        elif itemtype == 5:
+            queryItem = 'genre'
+        searchItem = queryItem + "Name"
+        query = ''''''
+        queryResult = []
+        #implement query
+        return queryResult
+
+
     # function checks for user input given a list of choices
     @staticmethod
     def get_choice(lst):
